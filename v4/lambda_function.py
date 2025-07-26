8d9203f4012987c77641153c697601c78a12cf15
Learning Serverless Security <joshua.arvin.lat+lss@gmail.com>
2025-07-26 14:43:30 +0800

    Create lambda_function.py

---
+++ v4/lambda_function.py
@@ -0,0 +1,60 @@
+import re
+import boto3
+import json
+
+from os import environ
+get_env = environ.get
+
+HARDCODED_KEY_01 = "ABCDE"
+HARDCODED_KEY_02 = "FGHIJ"
+
+
+def get_secret(secret_name, region_name="us-east-1"):
+    client = boto3.client("secretsmanager", region_name=region_name)
+    response = client.get_secret_value(SecretId=secret_name)
+    secret_dict = json.loads(response['SecretString'])
+
+    return secret_dict
+
+def get_statement(event):
+    params = event.get('queryStringParameters', {})
+    statement = params.get('statement', None)
+    
+    return statement
+
+
+def process_statement(statement):
+    output = "No statement parameter value provided"
+    
+    if statement:
+        output = eval(statement)
+    
+    return output
+
+
+def sanitize_output(output):
+    return re.sub(r'[^0-9\.]', '', str(output))
+
+
+def lambda_handler(event, context):
+    print("event: ", event)
+    print("HARDCODED_KEY_01: ", HARDCODED_KEY_01)
+    print("HARDCODED_KEY_02: ", HARDCODED_KEY_02)
+    print("CUSTOM_ENV_VAR_01: ", get_env('CUSTOM_ENV_VAR_01'))
+    print("CUSTOM_ENV_VAR_02: ", get_env('CUSTOM_ENV_VAR_02'))
+
+    sm_data = get_secret("prod/lambda-secret")
+    print("SECRETS MANAGER SECRETS: ", str(sm_data))
+    
+    statement = get_statement(event)
+    result = process_statement(statement)
+    
+    return {
+        'statusCode': 200,
+        'headers': {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET,OPTIONS",
+            "Access-Control-Allow-Headers": "*"
+        },
+        'body': sanitize_output(result)
+    }
