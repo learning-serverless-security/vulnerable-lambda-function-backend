633e219031f522cc3a01d8d71f55a4dc3f984c0f
Learning Serverless Security <joshua.arvin.lat+lss@gmail.com>
2025-07-26 14:44:07 +0800

    Create lambda_function.py

---
+++ v5/lambda_function.py
@@ -0,0 +1,85 @@
+import re
+import boto3
+import json
+import ast
+import operator
+from os import environ
+
+get_env = environ.get
+
+HARDCODED_KEY_01 = "ABCDE"
+HARDCODED_KEY_02 = "FGHIJ"
+
+operators = {
+    ast.Add: operator.add,
+    ast.Sub: operator.sub,
+    ast.Mult: operator.mul,
+    ast.Div: operator.truediv,
+    ast.Pow: operator.pow,
+    ast.Mod: operator.mod,
+    ast.USub: operator.neg,
+}
+
+
+def eval_expr(expr):
+    def _eval(node):
+        if isinstance(node, ast.Num):
+            return node.n
+        elif isinstance(node, ast.Constant):
+            return node.value
+        elif isinstance(node, ast.BinOp):
+            return operators[type(node.op)](_eval(node.left), _eval(node.right))
+        elif isinstance(node, ast.UnaryOp):
+            return operators[type(node.op)](_eval(node.operand))
+        else:
+            raise TypeError(f"Unsupported expression: {type(node).__name__}")
+    
+    try:
+        tree = ast.parse(expr, mode='eval')
+        return _eval(tree.body)
+    except Exception:
+        return "Invalid or unsafe expression"
+
+
+def process_statement(statement):
+    if not statement:
+        return "No statement parameter value provided"
+    return eval_expr(statement)
+
+
+def get_secret(secret_name, region_name="us-east-1"):
+    client = boto3.client("secretsmanager", region_name=region_name)
+    response = client.get_secret_value(SecretId=secret_name)
+    secret_dict = json.loads(response['SecretString'])
+
+    return secret_dict
+
+def get_statement(event):
+    params = event.get('queryStringParameters', {})
+    statement = params.get('statement', None)
+    
+    return statement
+
+
+def lambda_handler(event, context):
+    print("event: ", event)
+    print("HARDCODED_KEY_01: ", HARDCODED_KEY_01)
+    print("HARDCODED_KEY_02: ", HARDCODED_KEY_02)
+    print("CUSTOM_ENV_VAR_01: ", get_env('CUSTOM_ENV_VAR_01'))
+    print("CUSTOM_ENV_VAR_02: ", get_env('CUSTOM_ENV_VAR_02'))
+
+    sm_data = get_secret("prod/lambda-secret")
+    print("SECRETS MANAGER SECRETS: ", str(sm_data))
+    
+    statement = get_statement(event)
+    result = process_statement(statement)
+    
+    return {
+        'statusCode': 200,
+        'headers': {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET,OPTIONS",
+            "Access-Control-Allow-Headers": "*"
+        },
+        'body': result
+    }
