529bca510acd8d75f8dd54be1a600ae5ed2a92fa
Learning Serverless Security <joshua.arvin.lat+lss@gmail.com>
2025-07-26 14:41:17 +0800

    Update lambda_function.py

--- v2/lambda_function.py
+++ v2/lambda_function.py
@@ -1,1 +1,43 @@
+import re
 
+from os import environ
+get_env = environ.get
+
+HARDCODED_KEY_01 = "ABCDE"
+HARDCODED_KEY_02 = "FGHIJ"
+
+
+def get_statement(event):
+    params = event.get('queryStringParameters', {})
+    statement = params.get('statement', None)
+    
+    return statement
+
+
+def process_statement(statement):
+    output = "No statement parameter value provided"
+    
+    if statement:
+        output = eval(statement)
+    
+    return output
+
+
+def sanitize_output(output):
+    return re.sub(r'[^0-9\.]', '', str(output))
+
+
+def lambda_handler(event, context):
+    print("event: ", event)
+    print("HARDCODED_KEY_01: ", HARDCODED_KEY_01)
+    print("HARDCODED_KEY_02: ", HARDCODED_KEY_02)
+    print("CUSTOM_ENV_VAR_01: ", get_env('CUSTOM_ENV_VAR_01'))
+    print("CUSTOM_ENV_VAR_02: ", get_env('CUSTOM_ENV_VAR_02'))
+    
+    statement = get_statement(event)
+    result = process_statement(statement)
+    
+    return {
+        'statusCode': 200,
+        'body': sanitize_output(result)
+    }
